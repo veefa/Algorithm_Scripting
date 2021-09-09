36944a8d97f782004b46a17728ddb921e6fe8335
vefa <44091688+veefa@users.noreply.github.com>
2021-09-09 11:23:22 +0100

    Create sorted_Union.js

---
+++ sorted_Union.js
@@ -0,0 +1,18 @@
+//Problem
+/*
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+*/
+//
+////Solution
+//  let args = [...arguments];
+  let union = [];
+  for(let i = 0; i < args.length; i++){
+    for(let j = 0; j < args[i].length; j++){
+      console.log(union.indexOf())
+    }
+  }
+// return : [ 1, 3, 2, 5, 4 ]
