6c38abd486b82409aa8161a61d4d58003be31195
vefa <44091688+veefa@users.noreply.github.com>
2021-09-13 21:41:29 +0100

    Update sorted_Union.js

--- sorted_Union.js
+++ sorted_Union.js
@@ -10,17 +10,21 @@ The unique numbers should be sorted by their original order, but the final array
 ////Solution
 //
 function uniteUnique(arr) {
-  let args = [...arguments];
   let union = [];
-  for(let i = 0; i < args.length; i++){
-    for(let j = 0; j < args[i].length; j++){
+  //loop through the arguments
+  for (let i = 0; i < arguments.length; i++) {
+    let newArgs = arguments[i];
+    //loop through the array of arguments
+    for (var j = 0; j < newArgs.length; j++) {
+            var finalArgs = newArgs[j]
       //checking if each value exists in union. If it returns true, we can push() it into union. If the if it returns false, we move on to the next item
-      if(union.indexOf(args[i][j]) === -1){
-        union.push(args[i][j]);
+      if (union.indexOf(finalArgs) < 0) {
+                union.push(finalArgs); 
       }
     }
   }
   return union;
 }
 console.log(([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
 // return : [1, 3, 2, 5, 4]
